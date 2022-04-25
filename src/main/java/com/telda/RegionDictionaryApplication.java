4ff7823b949e2fb2886220ba93952653799c3185
GuardianOfPC <conik9999@mail.ru>
2022-04-25 13:29:19 +0300

    feat: add Spring properties and Gradle Settings

--- src/main/java/com/telda/RegionDictionaryApplication.java
+++ src/main/java/com/telda/RegionDictionaryApplication.java
@@ -1,9 +1,12 @@
-package com.telda.regiondictionary;
+package com.telda;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan("com.telda.mapper")
 public class RegionDictionaryApplication {
 
 	public static void main(String[] args) {
