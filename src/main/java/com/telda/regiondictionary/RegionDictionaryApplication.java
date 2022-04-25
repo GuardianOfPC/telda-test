34d4884a6f07dfe79356cc17d4fd4d74ec311d0c
GuardianOfPC <conik9999@mail.ru>
2022-04-25 10:22:49 +0300

    feat: init commit

---
+++ src/main/java/com/telda/regiondictionary/RegionDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.telda.regiondictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionDictionaryApplication.class, args);
+	}
+
+}
